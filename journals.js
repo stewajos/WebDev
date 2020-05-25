37cf2bdbbe3b12597bdd270cd73590f216a8f6da
MattOnGithub29 <brownma4@mail.gvsu.edu>
2020-05-25 12:27:21 -0400

    New endpoints files updated

---
+++ journals.js
@@ -0,0 +1,14 @@
+// All user endpoints in this file
+module.exports = function(app){
+	// Get users
+	app.get('/', (req, res) => {
+			const db = req.app.locals.db;
+			const collection = db.collection('journals');
+			collection.find().toArray(function(err, data){
+					//console.log(data);
+					res.send(data);
+					});
+			});
+			
+	// Put a user, create new one, delete, etc...
+};
--- users(2).js
+++ users(2).js
@@ -9,10 +9,6 @@ module.exports = function(app){
 					res.send(data);
 					});
 			});
-	app.set('/', (req, res) => {
-			const db = req.app.locals.db;
-			const collection = db.collection('users');
-			collection.set();
-	});
+			
 	// Put a user, create new one, delete, etc...
 };
